6c34623b7de5fe00d6d93c033c2d29fe805a8f48
Haksoo Ji <mintzzz1009@gmail.com>
2023-04-10 19:48:07 +0900

    [추가] admin 템플릿 커스텀을 위한 작업. admin 사이트 제목 변경

--- QnA_vote/settings.py
+++ QnA_vote/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'QnA_vote.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],   # 추가
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
